80e2e24013bff0f099ffd1a68dcf0f93f8d3cf8f
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-11-18 11:29:38 +0100

    axios installed and books data fetched

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      isLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    axios.get("https://api.myjson.com/bins/zyv02")
+      .then(res => {
+        this.setState({
+          isLoaded: true,
+          books: res.data.books
+        })
+      })
+  }
+
+  render() {
+    const { isLoaded, books } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+      return <div>hello world</div>
+    }
+  }
 }
 
 export default App;
