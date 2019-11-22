ffe34a5dd5e9c749ae435833f8c182dc1e456f2d
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-11-22 13:30:34 +0100

    Welcome modell done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,35 +1,44 @@
 import React from 'react'
 import '../styles/Hero.css'
 import Logo from '../img/fav.png'
+import Javascript from '../img/javascript.png'
+import css from '../img/css.png'
+import html from '../img/html.png'
+import react from '../img/react.png'
 
 function Hero() {
   return (
     <div className="hero">
       <div className="hero-content">
-        <h1>sign up for our news letter and recive crazy discounts</h1>
-        {/* <button className="hero-content_button">sign up</button> */}
+        <h1>the coolest app for bookworms out there!</h1>
+
         <p>
           <button data-fancybox data-src="#selectableModal" className="hero-content_button">about the app</button>
         </p>
 
-        <div style={{ 
-          display: "none", 
-          maxWidth: "80%", 
-          fontSize: "1.6rem", 
-          padding: "4rem", 
-          color:"white",
-          backgroundImage: "linear-gradient(to bottom, #d55ead, #543093)" }} id="selectableModal">
+        <div style={{
+          display: "none",
+          maxWidth: "60%",
+          fontSize: "2rem",
+          padding: "4rem",
+          color: "white",
+          backgroundImage: "linear-gradient(to bottom, #d55ead, #543093)"
+        }} id="selectableModal">
 
-          <h2 data-selectable="true" style={{ textAlign: "center" }}>Welcome!</h2>
-          <br />
-          <p data-selectable="true">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae fuga ipsum id assumenda. Quasi cumque unde accusantium minus error, quis quas, temporibus odio veritatis quaerat voluptates incidunt, nulla consequatur ducimus!
-          <br /><br />
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam sequi tenetur unde eveniet dolore iusto, vero vitae fugiat asperiores eius deleniti quae! Ea harum dolor minima laboriosam, assumenda repudiandae deleniti aperiam quaerat consequuntur ad, eius atque, eaque temporibus error aliquid! Earum illum iure ipsam molestias quaerat quisquam vel obcaecati quod?
-            <br /><br />
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod possimus natus numquam debitis molestiae minima ducimus quam rem facere sit qui perspiciatis laborum iure ipsum harum sint, aperiam aliquam iusto aut quasi repellat explicabo reprehenderit. Ad tempore itaque mollitia non, accusamus cum autem alias expedita provident ullam quo nihil. Animi, repellat maiores nulla nesciunt autem optio harum facilis cumque aliquam minima, quo esse illo numquam quod quas! Explicabo fuga necessitatibus magni distinctio libero voluptate deserunt, hic sequi repellendus expedita voluptas, debitis odit labore numquam architecto. Sit earum iste possimus, voluptatem maxime velit incidunt officiis eius illo aliquam non quidem. Aperiam totam corporis, ea voluptatum mollitia labore fugiat magni odio at tempora qui rem dolore dolorum quasi consectetur assumenda porro eos voluptatem? Quibusdam soluta alias at natus perspiciatis, sit iste, ut, delectus quae explicabo debitis! Accusamus placeat reiciendis animi inventore corporis mollitia veritatis esse numquam eius, consequatur ea voluptates eos doloremque voluptas dolor maxime tenetur, nemo rerum dolorum amet ex blanditiis, sint porro repellat. Corrupti repudiandae ratione eos ad iste necessitatibus dignissimos incidunt cupiditate, qui delectus, quibusdam explicabo sit asperiores rerum corporis, est fugit molestias porro quis magnam recusandae nostrum. Reiciendis aliquid aperiam sint repellendus expedita reprehenderit incidunt ut. Possimus, accusamus?
-          </p>
-          <br />
-          <img src={Logo} alt="logo" style={{ textAlign: "center", marginLeft: "50%", transform: "translateX(-50%)" }} />
+          <h2 data-selectable="true" style={{ textAlign: "center", paddingBottom: "2rem" }}>Welcome!</h2>
+
+          <p data-selectable="true">I've created this single page application using React. During my time at Ubiqum Code Academy this was one of the funnest projects. I've really enjoyed every moment of descovering this great framework!</p>
+
+          <h3 style={{ paddingTop: "2rem" }}>Special thanks to:</h3>
+
+          <div style={{ display: "flex", justifyContent: "space-around", alignItems: "center" }}>
+            <span style={{ fontFamily: "Pacifico, cursive", fontSize: "4rem", color: "#FF5268" }}>fancybox</span>
+            <img src={html} alt="html-logo" />
+            <img src={css} alt="css-logo" />
+            <img src={Javascript} alt="javascript-logo" />
+            <img src={react} alt="react-logo" />
+          </div>
+
         </div>
       </div >
     </div >
